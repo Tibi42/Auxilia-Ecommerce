bfc0d95e694223e2e27ce120a3e1b05b015e8956
Guillaume PECQUET <guillaume.pecquet@gmail.com>
2026-01-20 23:01:52 +0100

    MAJ BOotstrap

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,8 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * Contrôleur gérant l'affichage du catalogue de produits côté client
